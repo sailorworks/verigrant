7f3aaa030720c6f28af8673f705acf7e49fcc8f9
sailorworks <sahilpras11@gmail.com>
2025-06-28 20:44:27 +0530

    vercel changes 3

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,27 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack(config, options) {
+    const { isServer, nextRuntime } = options;
+
+    if (isServer && nextRuntime === "nodejs") {
+      config.module.rules.push({
+        test: /\.node$/,
+        use: [{ loader: "nextjs-node-loader" }],
+      });
+
+      config.externals = config.externals || [];
+      config.externals.push({
+        "@resvg/resvg-js-linux-x64-gnu":
+          "commonjs @resvg/resvg-js-linux-x64-gnu",
+        "@resvg/resvg-js-linux-x64-musl":
+          "commonjs @resvg/resvg-js-linux-x64-musl",
+      });
+    }
+
+    return config;
+  },
 };
 
 export default nextConfig;
